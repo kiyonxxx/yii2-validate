95e2c7143a09135808284da145b49c0b0164b4eb
Igor Tarasov <develop@dicr.org>
2019-01-05 03:11:26 +0500

    yii2-extension

--- src/DateFlagValidator.php
+++ src/DateFlagValidator.php
@@ -1,57 +1,78 @@
-<?php 
+<?php
 namespace dicr\validate;
 
 use yii\validators\Validator;
 
 /**
- * Валидатор данных типа флаг со значениями null/datetime, который конвертирует значения типа true в текущее 
+ * Валидатор данных типа флаг со значениями null/datetime, который конвертирует значения типа true в текущее
  * дату/время с заданным форматом.
- * 
+ *
  * Принимаемые значение:
  * false (null, 0, false, "", "false", "", "no", "off") => null,
  * true (1, true, "true", "yes", "on") => date(формат, time())
- * "Y-m-d H:i:s", int => date(формат, значение)   
- * 
+ * "Y-m-d H:i:s", int => date(формат, значение)
+ *
  * Используется с полями типа DATETIME/null, например в поле типа disabled, published, ....
- * 
+ *
  * @author Igor (Dicr) Tarasov <develop@dicr.org>
  * @version 180623
  */
 class DateFlagValidator extends Validator {
-	
+
 	/** @var string формат даты */
 	public $format = 'Y-m-d H:i:s';
-	
+
 	/** @var bool */
-	public $skipOnEmpty = false;
+	public $skipOnEmpty = true;
+
+	/**
+	 * Форматирует дату
+	 *
+	 * @param int $time значение
+	 * @return string
+	 */
+	protected function format(int $time=0) {
+	    return date($this->format, $time ?: null);
+	}
 
 	/**
 	 * {@inheritDoc}
 	 * @see \yii\validators\Validator::validateAttribute()
-	 */	
+	 */
 	public function validateAttribute($model, $attribute) {
-		$val = $model->{$attribute} ?? null;
-		
+		$val = $model[$attribute] ?? null;
+
 		// null or not exists
-		if (!isset($val)) return;
+		if (!isset($val)) {
+		    return;
+		}
 
 		if (empty($val)) {
 			$model->{$attribute} = null;
-		} else if (is_bool($val)) {
-			$model->{$attribute} = $val ? date($this->format) : null;
-		} else if (is_numeric("$val")) {
+		} elseif (is_bool($val)) {
+			$model->{$attribute} = $this->format();
+		} elseif (is_numeric("$val")) {
 			$val = (int)$val;
-			if ($val == 0) $model->{$attribute} = null;
-			else if ($val == 1) $model->$attribute = date($this->format);
-			else $model->{$attribute} = date($this->format, $val);
-		} else if (is_string($val)) {
+			if (empty($val)) {
+			    $model->{$attribute} = null;
+			} elseif ($val == 1) {
+			    $model->$attribute = $this->format();
+			} else {
+			    $model->{$attribute} = $this->format($val);
+			}
+		} elseif (is_string($val)) {
 			$val = trim($val);
-			if (in_array($val, ['', 'no', 'false', 'off'])) $model->{$attribute} = null;
-			else if (in_array($val, ['yes', 'true', 'on'])) $model->{$attribute} = date($this->format);
-			else {
+			if (in_array($val, ['', '0', 'no', 'false', 'off'])) {
+			    $model->{$attribute} = null;
+			} elseif (in_array($val, ['1', 'yes', 'true', 'on'])) {
+			    $model->{$attribute} = $this->format();
+			} else {
 				$tstamp = strtotime($val);
-				if (empty($tstamp)) $this->addError($model, $attribute, 'Некорректное значение флага/даты: '.$val);
-				else $model->{$attribute} = date($this->format, $tstamp);
+				if (empty($tstamp)) {
+				    $this->addError($model, $attribute, 'Некорректное значение флага/даты: '.$val);
+				} else {
+				    $model->{$attribute} = $this->format($tstamp);
+				}
 			}
 		} else {
 			$this->addError($model, $attribute, 'Неизвестный тип значения');
